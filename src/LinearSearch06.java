b890a2b14cdb88f3e2ec6dc51aad1e88a6b9168a
AJULUMEL <azruljoss@gmail.com>
2023-12-19 22:40:46 +0700

    jbsht9

---
+++ src/LinearSearch06.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class LinearSearch06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arrayInt;
+        int key = 0;
+        int hasil = 0;
+
+        System.out.print("Masukkan jumlah elemen array : ");
+        int n = input.nextInt();
+        arrayInt = new int[n];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan nilai elemen array ke-" + i + " : ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari :");
+        key = input.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+        if (hasil != 0) {
+            System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan");
+        }
+        input.close();
+    }
+}
